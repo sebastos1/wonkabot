e5249a17713b77f983cddba7344f6878cc572f25
Odd-Harald Lillestø Myhren <ohmyhren@uia.no>
2021-09-01 18:28:55 +0200

    latest

--- firststeps.py
+++ firststeps.py
@@ -1,18 +1,4 @@
 import discord
-from discord.ext import commands
-import logging
-
-logger = logging.getLogger("discord")
-logger.setLevel(logging.DEBUG)
-handler = logging.FileHandler(filename="discord.log", encoding="utf=8", mode="w")
-handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s"))
-logger.addHandler(handler)
-
-
-
-# bot =command.Bot(".")
-# @bot.listen()
-
 
 client = discord.Client()
 
@@ -20,20 +6,14 @@ client = discord.Client()
 async def on_ready():
     print("We logged in as {0.user}".format(client))
 
-
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    print(message)
+    # if message.bot:
+    #     await message.channel.send(message.content)
 
-    if message.content.startswith("$input"):
-        await message.channel.send("hei buddy")
-
-
-
-
-
-
-with open("botToken.txt") as f:
-    myToken = f.read()
+with open("botToken.txt") as file:
+    myToken = file.read()
 client.run(myToken)
